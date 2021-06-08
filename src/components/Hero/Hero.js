ae16a23b576c13780e01e8308cc4979f6e09383d
Aaron Carl Fernandez <actfernandez@mymail.mapua.edu.ph>
2021-06-08 16:44:15 +0800

    bad solution but it works

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
-import {Image} from 'react-bootstrap';
-import Image1 from '../../img/ezm_album_e.png';
+import {Container} from 'react-bootstrap';
+import Slider from "react-slick";
 
 export default class Main extends Component {
   
   render() {
+    var settings = {
+      dots: false,
+      infinite: true,
+      fade:true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      speed: 1000,
+      autoplaySpeed: 5000,
+      pauseOnHover: true,
+      arrows: false,
+      cssEase: "linear"
+    };
+
 
     return (
       <div className="backgroundPanel">
-        <Image src={Image1} className="backgroundImage" />
+        <Slider {...settings}>
+          <Container fluid className="hero-img-1"></Container>
+          <Container fluid className="hero-img-2"></Container>
+        </Slider>
       </div>
     )
   }
