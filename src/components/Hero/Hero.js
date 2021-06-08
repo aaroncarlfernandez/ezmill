a4de6b0f00707b27470fdd0831c689ebaceddf46
Aaron Carl Fernandez <actfernandez@mymail.mapua.edu.ph>
2021-06-08 15:51:42 +0800

    removed settings

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,20 +6,6 @@ export default class Main extends Component {
   
   render() {
 
-    var settings = {
-      dots: false,
-      infinite: true,
-      fade:true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      speed: 1000,
-      autoplaySpeed: 5000,
-      pauseOnHover: true,
-      arrows: false,
-      cssEase: "linear"
-    };
-
     return (
       <div className="backgroundPanel">
         <Image src={Image1} className="backgroundImage" />
